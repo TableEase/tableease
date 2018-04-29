69c09bb8e0bb14373b6e011315b3397462aef046
Avi Persin <avipersin@gmail.com>
2018-04-29 18:11:00 -0400

    Initial config

--- server/app.js
+++ server/app.js
@@ -48,7 +48,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
