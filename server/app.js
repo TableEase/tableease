25bed96bb8b6624d9d3e349fb4ab182eaa2beb40
Grahn Cooledge <gcooled@yahoo.com>
2018-05-10 11:48:55 -0400

    quick save for single quotes

--- server/app.js
+++ server/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const path = require("path");
-const logger = require("morgan");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
-const session = require("express-session");
-const MySQLStore = require("express-mysql-session")(session);
-const options = require("./config/db");
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 
-const morgan = require("morgan");
-const passport = require("passport");
-const flash = require("connect-flash");
+const morgan = require('morgan');
+const passport = require('passport');
+const flash = require('connect-flash');
 const router = express.Router({});
 
-require("./config/passport")(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
+
 const app = express();
 
 // view engine setup
@@ -23,72 +23,78 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(morgan("dev")); // log every request to the console
-app.use(logger("dev"));
+app.use(morgan('dev')); // log every request to the console
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public/js')));
 
-const angularDir = path.join(__dirname, "public/js/");
-app.use(express.static(angularDir));
-const sessionStore = new MySQLStore(options.dbConfig);
+const options = {
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'bfdecc39479008',
+  password: 'd9d922b1',
+  database: 'heroku_150cb394bbf6303'
+};
 
-app.use(session({
-  key: "session_cookie_name",
-  secret: "session_cookie_secret",
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false
-}));
+const sessionStore = new MySQLStore(options);
+
+app.use(
+  session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 // require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-const index = require("./routes/index");
-const login = require("./routes/login")(passport);
-const signup = require("./routes/signup")(passport);
-const profile = require("./routes/profile");
-const menu = require("./routes/menu");
-const logout = require("./routes/logout");
-const admin = require("./routes/admin");
-const users = require("./routes/users");
-const allergies = require("./routes/allergies");
+const index = require('./routes/index');
+const login = require('./routes/login')(passport);
+const signup = require('./routes/signup')(passport);
+const profile = require('./routes/profile');
+const menu = require('./routes/menu');
+const logout = require('./routes/logout');
+const admin = require('./routes/admin');
+const users = require('./routes/users');
 
-app.use("/api", router);
+app.use('/api', router);
 
-router.use("/", index);
-router.use("/login", login);
-router.use("/signup", signup);
-router.use("/profile", profile);
-router.use("/menu", menu);
-router.use("/logout", logout);
-router.use("/admin", admin);
-router.use("/users", users);
-router.use("/allergies", allergies);
+router.use('/', index);
+router.use('/login', login);
+router.use('/signup', signup);
+router.use('/profile', profile);
+router.use('/menu', menu);
+router.use('/logout', logout);
+router.use('/admin', admin);
+router.use('/users', users);
 
-app.get("/*", function(req, res, next) {
-  res.sendFile(angularDir + "/index.html");
+app.get('*', (req, res, next) => {
+  res.sendfile(path.join(__dirname, './public/js/index.html'));
 });
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   const err = new Error("Not Found");
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
