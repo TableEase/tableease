24d6461c0c0c09ad9b5316c46ff6162a30be33a7
Grahn Cooledge <gcooled@yahoo.com>
2018-05-17 13:22:40 -0400

    Created API folder and moved API rescources

--- server/api/config/passport.js
+++ server/api/config/passport.js
@@ -1,22 +1,20 @@
 // config/passport.js
 
 // load all the things we need
-const LocalStrategy = require("passport-local").Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
-const bcrypt = require("bcrypt-nodejs");
-const db = require("./db");
+const bcrypt = require('bcrypt-nodejs');
+const db = require('./db');
 
 // database models
-const company = new db({ tableName: "companies" });
+const company = new db({ tableName: 'companies' });
 const Company = db.extend({
-  tableName: "companies"
+  tableName: 'companies'
 });
-
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
-
   // =========================================================================
   // passport session setup ==================================================
   // =========================================================================
@@ -30,12 +28,16 @@ module.exports = function(passport) {
 
   // used to deserialize the user
   passport.deserializeUser(function(id, done) {
-    company.find("all", {
-      fields: ["name", "email", "id", "phone_number"],
-      where: "id = '" + id + "'"
-    }, function(err, rows) {
-      done(err, rows[0]);
-    });
+    company.find(
+      'all',
+      {
+        fields: ['name', 'email', 'id', 'phone_number'],
+        where: "id = '" + id + "'"
+      },
+      function(err, rows) {
+        done(err, rows[0]);
+      }
+    );
   });
 
   // =========================================================================
@@ -45,26 +47,32 @@ module.exports = function(passport) {
   // by default, if there was no name, it would just be called 'local'
 
   passport.use(
-    "local-signup",
-    new LocalStrategy({
+    'local-signup',
+    new LocalStrategy(
+      {
         // by default, local strategy uses username and password, we will override with email
-        usernameField: "email",
-        passwordField: "password",
+        usernameField: 'email',
+        passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
       },
       function(req, email, password, done) {
-
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        company.find("all", { where: "email = '" + email + "'" }, function(err, rows) {
-          if (err)
-            return done(err);
+        company.find('all', { where: "email = '" + email + "'" }, function(
+          err,
+          rows
+        ) {
+          if (err) return done(err);
           if (rows.length) {
-            return done(null, false, req.flash("signupMessage", "That email is already taken."));
+            return done(
+              null,
+              false,
+              req.flash('signupMessage', 'That email is already taken.')
+            );
           } else {
             const company = new Company({
               email: email,
-              password: bcrypt.hashSync(password, null, null),  // use the generateHash function in our user model
+              password: bcrypt.hashSync(password, null, null), // use the generateHash function in our user model
               phone_number: req.body.phoneNumber,
               name: req.body.name
             });
@@ -76,7 +84,8 @@ module.exports = function(passport) {
             });
           }
         });
-      })
+      }
+    )
   );
 
   // =========================================================================
@@ -86,28 +95,41 @@ module.exports = function(passport) {
   // by default, if there was no name, it would just be called 'local'
 
   passport.use(
-    "local-login",
+    'local-login',
 
-    new LocalStrategy({
+    new LocalStrategy(
+      {
         // by default, local strategy uses username and password, we will override with email
-        usernameField: "email",
-        passwordField: "password",
+        usernameField: 'email',
+        passwordField: 'password',
         passReqToCallback: true // allows us to pass back the entire request to the callback
       },
-      function(req, email, password, done) { // callback with email and password from our form
-        company.find("all", { where: "email = '" + email + "'" }, function(err, rows) {
-          if (err)
-            return done(err);
+      function(req, email, password, done) {
+        // callback with email and password from our form
+        company.find('all', { where: "email = '" + email + "'" }, function(
+          err,
+          rows
+        ) {
+          if (err) return done(err);
           if (!rows.length) {
-            return done(null, false, req.flash("loginMessage", "No user found.")); // req.flash is the way to set flashdata using connect-flash
+            return done(
+              null,
+              false,
+              req.flash('loginMessage', 'No user found.')
+            ); // req.flash is the way to set flashdata using connect-flash
           }
           // if the user is found but the password is wrong
           if (!bcrypt.compareSync(password, rows[0].password))
-            return done(null, false, req.flash("loginMessage", "Oops! Wrong password.")); // create the loginMessage and save it to session as flashdata
+            return done(
+              null,
+              false,
+              req.flash('loginMessage', 'Oops! Wrong password.')
+            ); // create the loginMessage and save it to session as flashdata
 
           // all is well, return successful user
           return done(null, rows[0]);
         });
-      })
+      }
+    )
   );
 };
